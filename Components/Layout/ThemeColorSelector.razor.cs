d04a61962f956ad7e515b08f79edb27677b351d1
mzdyhrave <info@hravemzdy.org>
2023-07-30 16:19:28 +0200

    create_manager_pages0

---
+++ Components/Layout/ThemeColorSelector.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagementApp.Components.Layout;
+
+public partial class ThemeColorSelector
+{
+    [Parameter]
+    public string? Value { get; set; }
+
+    [Parameter]
+    public EventCallback<string> ValueChanged { get; set; }
+
+    Task OnThemeColorSelect(string value)
+    {
+        Value = value;
+        return ValueChanged.InvokeAsync(value);
+    }
+}
---
+++ Components/Layout/TopMenu.razor.cs
@@ -0,0 +1,33 @@
+﻿using Blazorise.Localization;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagementApp.Components.Layout;
+
+public partial class TopMenu
+{
+    protected override async Task OnInitializedAsync()
+    {
+        await SelectCulture("en-US");
+
+        await base.OnInitializedAsync();
+    }
+
+    Task SelectCulture(string name)
+    {
+        LocalizationService!.ChangeLanguage(name);
+
+        return Task.CompletedTask;
+    }
+
+    [Parameter] public EventCallback<bool> ThemeEnabledChanged { get; set; }
+
+    [Parameter] public EventCallback<bool> ThemeGradientChanged { get; set; }
+
+    [Parameter] public EventCallback<bool> ThemeRoundedChanged { get; set; }
+
+    [Parameter] public EventCallback<string> ThemeColorChanged { get; set; }
+
+    [Inject] protected ITextLocalizerService? LocalizationService { get; set; }
+
+    [CascadingParameter] protected Theme? Theme { get; set; }
+}
